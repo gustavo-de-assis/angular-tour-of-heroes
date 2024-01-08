17bdf7b7735634a27f76b0eec79e1d510c2e050a
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-01-08 13:12:39 -0300

    feat: hero details component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Wolverine' },
+  { id: 13, name: 'NightCrawler' },
+  { id: 14, name: 'Cyclops' },
+  { id: 15, name: 'Beast' },
+  { id: 16, name: 'Storm' },
+  { id: 17, name: 'QuickSilver' },
+  { id: 18, name: 'Colossus' },
+  { id: 19, name: 'Jean Grey' },
+  { id: 20, name: 'Spike' },
+];
