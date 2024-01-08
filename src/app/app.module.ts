cb84284b6eafe8a15ee6211187ced978f8f778e0
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-01-08 15:32:04 -0300

    feat: adding dashboard and setting as default page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, MessagesComponent],
+  declarations: [AppComponent, MessagesComponent, DashboardComponent],
   imports: [BrowserModule, HeroDetailComponent, FormsModule, HeroesComponent, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
