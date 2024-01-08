1d9e757c5aca77acfcfdc61e33f3bce90f1a2be5
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-01-08 13:27:00 -0300

    feat: add heroes service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   standalone: true,
@@ -13,8 +13,18 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   imports: [FormsModule, NgIf, NgFor, UpperCasePipe, HeroDetailComponent],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
